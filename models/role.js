5dd87b62384a1251afae51efa8652f6b39919b81
a-toro <wtorobenavides@gmail.com>
2022-08-18 21:18:48 -0500

    Add primary key for column id

--- models/role.js
+++ models/role.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
   Role.init({
     id: {
       allowNull: false,
+      primaryKey: true,
       autoIncrement: true,
       type: DataTypes.INTEGER
     },
