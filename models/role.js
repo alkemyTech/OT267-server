eda90f944b038def3a426ed83172523ce533cc33
a-toro <wtorobenavides@gmail.com>
2022-08-19 10:23:01 -0500

    Revert "Defined association with the model User"
    This reverts commit 62a1aec21015a716dc380b624a81b9409549d378.

--- models/role.js
+++ models/role.js
@@ -9,10 +9,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Role.hasMany(models.User, { 
-        foreignKey: "roleId",
-        sourceKey: "id"
-      }); 
     }
   };
   Role.init({
