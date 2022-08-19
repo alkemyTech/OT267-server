62a1aec21015a716dc380b624a81b9409549d378
a-toro <wtorobenavides@gmail.com>
2022-08-19 10:01:37 -0500

    Defined association with the model User

--- models/role.js
+++ models/role.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Role.hasMany(models.User, { 
+        foreignKey: "roleId",
+        sourceKey: "id"
+      }); 
     }
   };
   Role.init({
