aec2f2b5cb33faf3f9231a9c34ce82cc436f949e
Soledad Marini <msmarinip@gmail.com>
2022-08-22 17:27:14 -0300

    https://alkemy-labs.atlassian.net/browse/OT267-33 Añadir lógica de JWT

---
+++ middlewares/validateJWT.js
@@ -0,0 +1,37 @@
+const { response } = require("express")
+const jwt = require('jsonwebtoken')
+
+
+const validateJWT = (req, res = response, next) => {
+    const token = req.header('x-token');
+    if (!token) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Token is requeried'
+        })
+    }
+
+    try {
+        const { uid, name } = jwt.verify(
+            token,
+            process.env.SECRET_JWT_SEED
+        );
+
+        req.uid = uid;
+        req.name = name;
+        req.role = role;
+        req.email = email;
+
+
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: 'Not valid Token'
+        });
+    }
+    next();
+}
+
+module.exports = {
+    validateJWT
+}
