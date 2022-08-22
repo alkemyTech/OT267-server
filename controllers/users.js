7e9650b4c31420b6aff02533eaacf38bbfa296e9
francisco martin lynch <lynch_francisco_w@hotmail.com>
2022-08-22 16:11:22 -0300

    feat: login authentication

---
+++ controllers/users.js
@@ -0,0 +1,59 @@
+const { findUsers } = require('../services/users');
+const { body, validationResult } = require('express-validator');
+const { comparePassword } = require('../helpers/bcrypt');
+const e = require('express');
+
+const login = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    body(email).isEmail(),
+        body(password).isLength({ min: 8 }).matches(/\d/).matches('[A-Z]').trim().trim()
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const userFound = await findUsers(email)
+
+        if (!userFound) {
+            res.status(404).send('Email not registered');
+        }
+
+        await comparePassword(password, user.password)
+            .then(result => {
+                if (result) {
+                    res.status(201).json({
+                        msg: 'Login successfully',
+                        user: userFound
+                    });
+                } else {
+                    res.status(404).json({
+                        msg: 'The password is wrong',
+                    });
+                }
+            })
+            .catch(error => {
+                res.status(404).json({
+                    ok: false,
+                    error: error
+                });
+            });
+
+    } catch (error) {
+
+        res.status(404).json({
+            ok: false,
+            error: error
+        });
+
+    }
+}
+
+module.exports = {
+    login
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { login } = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
-});
+}); 
+
+router.post('/auth/login', login);
 
 module.exports = router;
