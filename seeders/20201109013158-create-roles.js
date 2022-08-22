c86f487e6131cdcbc202aa274c6cade9882c42a3
a-toro <wtorobenavides@gmail.com>
2022-08-22 07:46:03 -0500

    Add logic fot reverting the changes of the model Role

--- seeders/20201109013158-create-roles.js
+++ seeders/20201109013158-create-roles.js
@@ -19,10 +19,8 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
+     * Revert seed for the model Roles.
      */
+    return queryInterface.bulkDelete('Roles', null, {});
   }
 };
