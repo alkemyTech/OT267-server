e9e67ca7cdfd9379d45aeee8bd8225747040f3af
francisco martin lynch <lynch_francisco_w@hotmail.com>
2022-08-22 13:22:00 -0300

    fix: comments removed

--- seeders/20220820162211-create-demo-categorie.js
+++ seeders/20220820162211-create-demo-categorie.js
@@ -2,22 +2,16 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Categories', [
+      {
+        name: 'Local',
+        description: null,
+        image: null,
+        createdAt: new Date,
+        updatedAt: new Date
+      }], {});
   },
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Categories', null, {});
   }
 };
