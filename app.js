fa15892dc831c8bb1e2ebc360687baa18c8f0a03
Alfredo Blanco <alfre.blanco@icloud.com>
2022-08-22 15:36:11 -0300

    https://alkemy-labs.atlassian.net/jira/software/c/projects/OT267/boards/362?modal=detail&selectedIssue=OT267-20

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use("/activities", activitiesRouter);
 app.use('/roles', rolesRouter);
 app.use('/organizations', organizationsRouter);
 app.use('/testimonials', testimonialsRouter)
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
