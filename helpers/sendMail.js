dec9f797a656ae6a0a906ebb9f4b6859a8c2a9e8
Soledad Marini <msmarinip@gmail.com>
2022-08-22 12:46:59 -0300

    https://alkemy-labs.atlassian.net/browse/OT267-29 Integrar envío de emails

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendMail = async (email, subject = null, text = null) => {
     if (!email) throw 'El email es requerido.'
     if (!subject) subject = 'Contacto - Somos Más-';
-    if (!text) text = 'Hemos recibido su consulta';
+    if (!text) text = 'Hemos recibido su consulta. Muchas gracias.';
     const msg = {
         to: email,
         from: process.env.EMAIL_SOMOSMAS,
@@ -16,15 +16,25 @@ const sendMail = async (email, subject = null, text = null) => {
     try {
         await sgMail.send(msg);
         console.log('Message sent succesfully')
+        return {
+            status: true,
+            message: 'Mail enviado existosamente'
+        }
     } catch (error) {
+        let message = 'Error: No se pudo enviar el mail';
         console.log(error);
         if (error.response) {
             console.error(error.response)
+            message = error.response;
+        }
+        return {
+            status: false,
+            message
         }
     }
 }
 
-// sendMail('msmarinip@gmail.com');
+
 
 module.exports = {
     sendMail
