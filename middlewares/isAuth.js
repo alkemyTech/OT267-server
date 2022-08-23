d2160319b920741a20d4b51ae5abee9c3255bb1e
BrunoBohlmann <brunobohlma25@gmail.com>
2022-08-23 14:44:50 -0300

    isAuth middleware almost complete

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,3 +1,31 @@
-const isAuth = (req, res, next) => {
+const { includes } = require("lodash");
+const { validateJWT } = require("../helpers/jwt");
+const { User } = require("../models/user");
+
+const isAuth = async (req, res, next) => {
   const token = req.header["x-token"];
+
+  const { uid, name, status, message } = validateJWT(token);
+
+  if (status === false) {
+    return res.status(403).json({
+      status,
+      message,
+      data: {},
+    });
+  }
+
+  const userExists = await User.findByPk(id, { include: "roleId" });
+
+  if (!userExists) {
+    return res.status(403).json({
+      status: false,
+      message: "User not exists",
+      data: {},
+    });
+  }
+
+  req.user = userExists;
+
+  next();
 };
