7265b90d7c2084ef602a6695c31bd628b2b79021
Elias Pedevilla <pedevillae@gmail.com>
2022-08-25 15:49:52 -0500

    fix: add try catch to deleteSingleUser controller and add return in deleteUser function

--- controllers/users.js
+++ controllers/users.js
@@ -70,12 +70,16 @@ const getAllUsers = async (req, res) => {
 
 const deleteSingleUser = async (_req, res, next) => {
   const { id } = _req.params;
-  const data = await deleteUser(id);
+  try {
+    const data = await deleteUser(id);
 
-  if (data === 0) {
-    return res.status(404).json({
-      message: 'user does not exist',
-    });
+    if (data === 0) {
+      return res.status(404).json({
+        message: 'user does not exist',
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 
   return res.status(200).json({
