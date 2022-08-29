1a8b0b343b93250312bc7d3a0f389ac8a918458b
Alfredo Blanco <alfre.blanco@icloud.com>
2022-08-29 17:46:01 -0300

    [OT267-54]Endpoint actualizar actividad

--- routes/activities.js
+++ routes/activities.js
@@ -2,6 +2,9 @@
 // Default imports
 const { query } = require('express');
 const express = require('express');
+const { updateActivity } = require('../controllers/activity');
+const { isAdmin } = require('../middlewares/isAdmin');
+const { isAuth } = require('../middlewares/isAuth');
 
 const router = express.Router();
 
@@ -9,5 +12,7 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   res.send('respond with a resource');
 });
+
+router.put('/:id', isAuth, isAdmin, updateActivity);
 
 module.exports = router;
