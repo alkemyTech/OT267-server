3c90890ee78e80502e946297b426dcd586cc5653
a-toro <wtorobenavides@gmail.com>
2022-08-30 15:58:23 -0500

    feat: add routes for update categories

--- routes/categories.js
+++ routes/categories.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getCategoryById, deleteSingleCategory, createNewCategory, getAllCategoriesName,
+  getCategoryById, deleteSingleCategory, createNewCategory, getAllCategoriesName, update,
 } = require('../controllers/category');
 const { validateCategoryById } = require('../middlewares/category');
 const { isAdmin } = require('../middlewares/isAdmin');
@@ -14,5 +14,6 @@ router.get('/:id', validateCategoryById, getCategoryById);
 router.post('/', [isAuth, isAdmin], createNewCategory);
 router.delete('/:id', isAuth, isAdmin, deleteSingleCategory);
 router.get('/', isAuth, isAdmin, getAllCategoriesName);
+router.put('/:id', isAuth, isAdmin, update);
 
 module.exports = router;
