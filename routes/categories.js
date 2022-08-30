b7fc63ea0c3d989402e8bddfd222aca738b0122a
a-toro <wtorobenavides@gmail.com>
2022-08-30 08:26:46 -0500

    feat: add route for get all categories name

--- routes/categories.js
+++ routes/categories.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const { deleteSingleCategory } = require('../controllers/category');
+const { deleteSingleCategory, getAllCategoriesName } = require('../controllers/category');
 const { isAdmin } = require('../middlewares/isAdmin');
 const { isAuth } = require('../middlewares/isAuth');
 
-router.delete('/:id',isAuth, isAdmin, deleteSingleCategory);
+router.delete('/:id', isAuth, isAdmin, deleteSingleCategory);
+router.get('/', isAuth, isAdmin, getAllCategoriesName);
 
 module.exports = router;
