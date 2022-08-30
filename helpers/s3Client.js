0957a7557b0e68003798ce868d14e4926dc47fc6
a-toro <wtorobenavides@gmail.com>
2022-08-29 23:10:46 -0500

    Setup amazon S3 SDK

---
+++ helpers/s3Client.js
@@ -0,0 +1,10 @@
+const S3 = require('aws-sdk/clients/s3');
+const accessKeyId = process.env.aws_access_key_id;
+const secretAccessKey = process.env.aws_secret_access_key;
+
+const s3Client = new S3({
+  accessKeyId,
+  secretAccessKey,
+});
+
+module.exports = { s3Client };
