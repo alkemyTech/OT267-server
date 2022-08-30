30deab4deba50c514d79d0b726a3cd51386e4868
Alfredo Blanco <alfre.blanco@icloud.com>
2022-08-30 12:48:44 -0300

    [OT267-55]Endpoint actualizar actividad

--- routes/activities.js
+++ routes/activities.js
@@ -11,9 +11,6 @@ const { validateActivity } = require('../middlewares/validateActivity');
 
 const { getAllActivities, createActivity } = require('../controllers/activities');
 
-const { isAdmin } = require('../middlewares/isAdmin');
-const { isAuth } = require('../middlewares/isAuth');
-
 router.get('/', isAuth, isAdmin, getAllActivities);
 
 router.post('/', isAuth, isAdmin, validateActivity, createActivity);
