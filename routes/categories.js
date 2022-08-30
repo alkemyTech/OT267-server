48db090c4fdf043e8204dc69bd8736e3973ba9f4
a-toro <wtorobenavides@gmail.com>
2022-08-30 12:39:58 -0500

    feat: add route for update category

--- routes/categories.js
+++ routes/categories.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { deleteSingleCategory } = require('../controllers/category');
+const { deleteSingleCategory, update } = require('../controllers/category');
 const { isAdmin } = require('../middlewares/isAdmin');
 const { isAuth } = require('../middlewares/isAuth');
 
-router.delete('/:id',isAuth, isAdmin, deleteSingleCategory);
-
+router.delete('/:id', isAuth, isAdmin, deleteSingleCategory);
+router.put('/:id', isAuth, isAdmin, update);
 module.exports = router;
