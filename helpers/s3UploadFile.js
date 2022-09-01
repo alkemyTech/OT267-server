5bda29e1fc75c04de11ff47203ddfef5c36d39cf
a-toro <wtorobenavides@gmail.com>
2022-08-31 21:16:26 -0500

    feat: create service for save files in aws s3

---
+++ helpers/s3UploadFile.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const { s3Client } = require('./s3Client');
+
+const bucketNameDefault = process.env.AWS_BUCKET_NAME;
+
+/**
+ * This function storage files in aws s3 services.
+ * @param {Object} file A file object.
+ * @param {String} bucketName Name of the bucket.
+ * @returns A promise with the file location url.
+ */
+const s3UploadFile = (file, bucketName = bucketNameDefault) => {
+  // Configure the file stream and obtain the upload parameters
+  const fileStream = fs.createReadStream(file.tempFilePath);
+  const uploadParams = {
+    Bucket: bucketName,
+    Key: file.name,
+    Body: fileStream,
+  };
+
+  return new Promise((resolve, reject) => {
+    // call S3 to retrieve upload file to specified bucket
+    s3Client.upload(uploadParams, (error, data) => {
+      if (!error) resolve(data.Location);
+      reject(error);
+    });
+  });
+};
+
+module.exports = { s3UploadFile };
