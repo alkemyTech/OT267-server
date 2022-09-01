5f63c3df90e87dbe0a773a58a05fdc1535540787
a-toro <wtorobenavides@gmail.com>
2022-09-01 09:41:09 -0500

    feat: create services for register new contacts

---
+++ services/contact.js
@@ -0,0 +1,20 @@
+const { Contact } = require('../models');
+/**
+ * Create a new contact in the database.
+ * @param {Object} param0 Object with the data.
+ * @returns the information about the new contact create in the database.
+ */
+// eslint-disable-next-line object-curly-newline
+const createUser = async ({ name, phone, email, message }) => {
+  const newContact = await Contact.create({
+    name,
+    phone,
+    email,
+    message,
+  });
+  return newContact;
+};
+
+module.exports = {
+  createUser,
+};
