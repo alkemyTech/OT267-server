d5d53800624bc8bd680501418f7e12a251219834
BrunoBohlmann <brunobohlma25@gmail.com>
2022-09-02 22:14:08 -0300

    Feat: create member made, route, controller, service and validate

---
+++ validators/validateMembers.js
@@ -0,0 +1,37 @@
+const { check } = require('express-validator');
+const { handleResult } = require('../middlewares/validateFields');
+
+const validateCreateMembers = [
+  check('name', 'Ingrese el nombre de la novedad')
+    .exists()
+    .isLength({ min: 1 })
+    .trim()
+    .escape(),
+
+  check(['linkedinUrl', 'instagramUrl, facebookurl'], 'Ingrese una url valida')
+    .optional({ checkFalsy: true })
+    .isURL()
+    .trim()
+    .escape(),
+
+  check('image', 'Ingrese una URL de imagen')
+    .optional({ checkFalsy: true })
+    .isURL()
+    .isLength({ min: 1 })
+    .trim(),
+
+  check('description', 'Ingrese una descripcion')
+    .exists()
+    .isLength({ min: 15 })
+    .withMessage('La descripcion es muy corta')
+    .trim()
+    .escape(),
+
+  (req, res, next) => {
+    handleResult(req, res, next);
+  },
+];
+
+module.exports = {
+  validateCreateMembers,
+};
