df0cb50d9e047d363317d71af8e0b708a18b9495
Soledad Marini <msmarinip@gmail.com>
2022-09-11 10:11:23 -0300

    https://alkemy-labs.atlassian.net/browse/OT267-88 Agregar paginación a Miembros

---
+++ helpers/paginator.js
@@ -0,0 +1,53 @@
+const paginator = async (req, model, urlmodel) => {
+  // endpoint
+  const url = `http://localhost:3000/${urlmodel}?page=`;
+
+  // pages
+  const getNextPage = (page, limit, total) => {
+    if ((total / limit) > page) {
+      return url + (page + 1);
+    }
+    return null;
+  };
+
+  const getPreviousPage = (page, limit, total) => {
+    if ((total / limit) < page) return url + 1;
+    if (page <= 1) return null;
+    return url + (page - 1);
+  };
+
+  // getOffset
+  const getOffset = (page, limit) => (page * limit) - limit;
+
+  // current page
+  let page = 1;
+  if (!Number.isNaN(Number(req.query.page))) { page = Number(req.query.page); }
+
+  // rows per pagina
+  const limit = 10;
+
+  // request
+  const options = {
+    offset: getOffset(page, limit),
+    limit,
+  };
+
+  const { count, rows } = await model.findAndCountAll(options);
+
+  // total pages
+  const totalPages = Math.ceil(count / limit);
+
+  return {
+    totalPages,
+    previousPage: getPreviousPage(page, limit, count),
+    currentPage: page > totalPages ? 'page does not exist' : page,
+    nextPage: getNextPage(page, limit, count),
+    totalRows: count,
+    rowsPerPage: limit,
+    rows,
+  };
+};
+
+module.exports = {
+  paginator,
+};
