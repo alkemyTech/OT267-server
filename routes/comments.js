394e299dc6b73062a23dcaf445c54f8d9fe70af1
a-toro <wtorobenavides@gmail.com>
2022-09-11 18:07:47 -0500

    feat: add route for delete comment by Id

--- routes/comments.js
+++ routes/comments.js
@@ -5,9 +5,10 @@ const router = express.Router();
 const { isAuth } = require('../middlewares');
 
 const { createNewComment } = require('../controllers/comments');
-
+const { deleteSingleComment } = require('../controllers/comments');
 const { validateFields } = require('../validators/validateComment');
 
 router.post('/', isAuth, validateFields, createNewComment);
+router.delete('/:id', isAuth, deleteSingleComment);
 
 module.exports = router;
