dad68ca0dfbc7f3d5c0f58628a692dd69828f8b4
a-toro <wtorobenavides@gmail.com>
2022-09-12 09:07:47 -0500

    feat: add controller for delete a comment by id

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,5 @@
-const { success, serverError } = require('../helpers');
-const { createComment } = require('../services/comment');
+const { success, serverError, error } = require('../helpers');
+const { createComment, deleteComment } = require('../services/comment');
 
 const createNewComment = async (req, res) => {
   try {
@@ -15,4 +15,27 @@ const createNewComment = async (req, res) => {
   }
 };
 
-module.exports = { createNewComment };
+const deleteSingleComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await deleteComment(id);
+    if (response === 0) return error({ res, message: 'comment nof found' });
+
+    success({
+      res,
+      message: 'comment delete',
+      status: 200,
+    });
+  } catch (err) {
+    serverError({
+      res,
+      message: err.message,
+      status: 500,
+    });
+  }
+};
+
+module.exports = {
+  createNewComment,
+  deleteSingleComment,
+};
