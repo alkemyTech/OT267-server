0f621be4016b2fdc78505005ebcb1a3efd3024a4
Alfredo Blanco <alfre.blanco@icloud.com>
2022-09-12 11:34:39 -0300

    Uso de sequelize para ordenar

--- services/comment.js
+++ services/comment.js
@@ -1,13 +1,13 @@
 const { Comment } = require('../models/index');
 
-const getComments = async () => {
-  const data = await Comment.findAll({});
-  const response = data.sort((a, b) => {
-    if (b.createdAt < a.createdAt) return 1;
-    return -1;
-  }).map((e) => e.body);
-  return response;
-};
+const getComments = () => Comment.findAll({
+  order: [
+    ['createdAT', 'ASC'],
+  ],
+  attributes: [
+    'body',
+  ],
+});
 
 const createComment = async (data) => Comment.create({
   ...data,
