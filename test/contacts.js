34555ac48a8aea409cb73a51b062bdcdd5230d49
a-toro <wtorobenavides@gmail.com>
2022-09-20 15:03:14 -0500

    Add test for get contact with auth

--- test/contacts.js
+++ test/contacts.js
@@ -17,7 +17,7 @@ const adminToken = process.env.TEST_ADMIN_TOKEN;
 
 describe('Testing contacts endpoints', () => {
 
-    describe('# GET /contacts', () => {
+    describe('#Get contacts: GET /contacts', () => {
         // Test auth
         it('Shoul return and error for not auth user', (done) => {
             chai.request(app)
@@ -29,5 +29,19 @@ describe('Testing contacts endpoints', () => {
                 });
         });
 
+        // Test get contacts
+        it('Should return the list of contacts', (done) => {
+            chai.request(app)
+                .get('/contacts')
+                .set({ Authorization: adminToken})
+                .end((error, res) => {
+                    expect(error).to.be.null;
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('list of all contacts');
+                    expect(res.body).to.have.property('data');
+                    expect(res.body.data).to.be.a('array');
+                    done();
+                });
+        });
     });
 });
