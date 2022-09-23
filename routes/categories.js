823824ec85aa8b73e05a1dffe3dbcbb2846cb5f6
Zayra Velasco <zayra.contacto@gmail.com>
2022-09-23 06:49:57 -0300

    refact testimonies and categories documentary components names

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const { isAdmin, isAuth, uploadFile } = require('../middlewares');
+
 const {
   deleteCategory,
   getCategory,
@@ -10,16 +11,396 @@ const {
   createCategory,
   updateCategory,
 } = require('../controllers/categories');
+
 const { validateCategoryId, validateNewsFields } = require('../validators/validateCategory');
 
-router.get('/:id', isAuth, isAdmin, validateCategoryId, getCategory);
+/**
+ * @swagger
+ * tags:
+ *   - name: Category
+ *     description: News Category
+ * components:
+ *   schemas:
+ *     Category Model:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: category name
+ *         description:
+ *           type: string
+ *           description: category description
+ *         image:
+ *           type: string
+ *           description: category image url
+ *         deletedAt:
+ *           type: date
+ *           description: delete date - softdelete
+ *         createdAt:
+ *           type: date
+ *           description: creation date
+ *         updatedAt:
+ *           type: date
+ *           description: update date
+ *       required:
+ *         - name
+ *       example:
+ *         name: Category name
+ *         description: Category description
+ *         image: https://category-image.jpg
+ *     CategoryResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: response title
+ *         data:
+ *           type: object
+ *           description: response content
+ *           properties:
+ *             id:
+ *               type: integer
+ *               description: category id
+ *             name:
+ *               type: string
+ *               description: category name
+ *             description:
+ *               type: string
+ *               description: category description
+ *             image:
+ *               type: string
+ *               description: category image url
+ *             createdAt:
+ *               type: date
+ *               description: creation date
+ *             updatedAt:
+ *               type: date
+ *               description: update date
+ *     CategoryRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: category name
+ *           required: true
+ *         description:
+ *           type: string
+ *           description: category description
+ *         image:
+ *           type: string
+ *           description: category image url
+ *       example:
+ *         name: Category name
+ *         description: Category description
+ *         image: https://category-image.jpg
+ *     CategoryPutRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: category name
+ *         description:
+ *           type: string
+ *           description: category description
+ *         image:
+ *           type: string
+ *           description: category image url
+ *       example:
+ *         name: Category name
+ *         description: Category description
+ *         image: https://category-image.jpg
+ *     Categories:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: category id
+ *         name:
+ *           type: string
+ *           description: category name
+ *     CategoriesAndPagination:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: response title
+ *         data:
+ *           type: object
+ *           description: response content
+ *           properties:
+ *             totalPages:
+ *               type: integer
+ *               description: total number of response pages
+ *             previousPage:
+ *               type: integer
+ *               description: response previous page
+ *             currentPage:
+ *               type: integer
+ *               description: response current page
+ *             nextPage:
+ *               type: integer
+ *               description: response next page
+ *             totalRows:
+ *               type: integer
+ *               description: total number of response rows
+ *             rowsPerPage:
+ *               type: integer
+ *               description: number of response rows per page
+ *             rows:
+ *               type: array
+ *               description: current page rows
+ *               items:
+ *                 $ref: '#/components/schemas/Categories'
+ *     CategoryMessageResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: message
+ *   responses:
+ *     getAllCategories:
+ *       description: list of all categories
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CategoriesAndPagination'
+ *           example:
+ *             message: list of all categories
+ *             data:
+ *               totalPages: 1
+ *               previousPage: null
+ *               currentPage: 1
+ *               nextPage: null
+ *               totalRows: 2
+ *               rowsPerPage: 10
+ *               rows:
+ *                 - id: 1
+ *                   name: Category name
+ *                 - id: 2
+ *                   name: Category name
+ *     getSingleCategory:
+ *       description: category detail
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CategoryResponse'
+ *           example:
+ *             message: category detail
+ *             data:
+ *               id: 1
+ *               name: Category name
+ *               description: Category description
+ *               image: https://category-image.jpg
+ *               createdAt: 2022-09-14T17:09:49.000Z
+ *               updatedAt: 2022-09-14T17:09:49.000Z
+ *     deleteCategory:
+ *       description: category deleted
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CategoryMessageResponse'
+ *           example:
+ *             message: category deleted
+ *     createCategory:
+ *       description: category created
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CategoryResponse'
+ *           example:
+ *             message: category created
+ *             data:
+ *               id: 1
+ *               name: Category name
+ *               description: Category description
+ *               image: https://category-image.jpg
+ *               createdAt: 2022-09-14T17:09:49.000Z
+ *               updatedAt: 2022-09-14T17:09:49.000Z
+ *     updateCategory:
+ *       description: category updated
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CategoryResponse'
+ *           example:
+ *             message: category updated
+ *             data:
+ *               id: 1
+ *               name: Category name
+ *               description: Category description
+ *               image: https://category-image.jpg
+ *               createdAt: 2022-09-14T17:09:49.000Z
+ *               updatedAt: 2022-09-14T17:09:49.000Z
+ *     notFoundCategory:
+ *       description: category not found
+ *     badRequestCategory:
+ *       description: category already exists
+ *     401:
+ *       description: unauthorized - id is required
+ *     403:
+ *       description: forbidden - admin access is required - validation error
+ *     500:
+ *       description: server error
+ *   parameters:
+ *     idCategory:
+ *       in: path
+ *       name: id
+ *       schema:
+ *         type: integer
+ *       required: true
+ *       description: category id
+ *     page:
+ *       in: query
+ *       name: page
+ *       schema:
+ *         type: integer
+ *       description: page required
+ *     by:
+ *       in: query
+ *       name: by
+ *       schema:
+ *         type: string
+ *       description: criteria to order rows
+ *     order:
+ *       in: query
+ *       name: order
+ *       schema:
+ *         type: string
+ *       description: direction to order rows
+ *   securitySchemes:
+ *     ApiKeyAuth:
+ *       in: header
+ *       name: authorization
+ *       type: apiKey
+ */
 
-router.post('/', isAuth, isAdmin, validateNewsFields, uploadFile, createCategory);
-
-router.delete('/:id', isAuth, isAdmin, validateCategoryId, deleteCategory);
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     security:
+ *       - ApiKeyAuth: []
+ *     summary: Get all categories
+ *     tags: [Category]
+ *     parameters:
+ *     - $ref: '#/components/parameters/page'
+ *     - $ref: '#/components/parameters/by'
+ *     - $ref: '#/components/parameters/order'
+ *     responses:
+ *       200:
+ *         $ref: '#/components/responses/getAllCategories'
+ *       401:
+ *         $ref: '#/components/responses/401'
+ *       403:
+ *         $ref: '#/components/responses/403'
+ *       500:
+ *         $ref: '#/components/responses/500'
+ */
 
 router.get('/', isAuth, isAdmin, getCategories);
 
+/**
+ * @swagger
+ * /categories/{id}:
+ *    get:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Get a category detail
+ *      tags: [Category]
+ *      parameters:
+ *      - $ref: '#/components/parameters/idCategory'
+ *      responses:
+ *       200:
+ *         $ref: '#/components/responses/getSingleCategory'
+ *       401:
+ *         $ref: '#/components/responses/401'
+ *       403:
+ *         $ref: '#/components/responses/403'
+ *       500:
+ *         $ref: '#/components/responses/500'
+ */
+
+router.get('/:id', isAuth, isAdmin, validateCategoryId, getCategory);
+
+/**
+ * @swagger
+ * /categories:
+ *    post:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Create a category
+ *      tags: [Category]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CategoryRequest'
+ *      responses:
+ *        201:
+ *          $ref: '#/components/responses/createCategory'
+ *        400:
+ *          $ref: '#/components/responses/badRequestCategory'
+ *        401:
+ *          $ref: '#/components/responses/401'
+ *        403:
+ *          $ref: '#/components/responses/403'
+ *        500:
+ *          $ref: '#/components/responses/500'
+ */
+
+router.post('/', isAuth, isAdmin, validateNewsFields, uploadFile, createCategory);
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *    put:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Update a category
+ *      tags: [Category]
+ *      parameters:
+ *      - $ref: '#/components/parameters/idCategory'
+ *      requestBody:
+ *        required: true
+ *        content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/CategoryPutRequest'
+ *      responses:
+ *        201:
+ *          $ref: '#/components/responses/updateCategory'
+ *        401:
+ *          $ref: '#/components/responses/401'
+ *        403:
+ *          $ref: '#/components/responses/403'
+ *        500:
+ *          $ref: '#/components/responses/500'
+ */
+
 router.put('/:id', isAuth, isAdmin, validateCategoryId, uploadFile, updateCategory);
+
+/**
+ * @swagger
+ * /categories/{id}:
+ *    delete:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Delete a category
+ *      tags: [Category]
+ *      parameters:
+ *      - $ref: '#/components/parameters/idCategory'
+ *      responses:
+ *        200:
+ *          $ref: '#/components/responses/deleteCategory'
+ *        401:
+ *          $ref: '#/components/responses/401'
+ *        403:
+ *          $ref: '#/components/responses/403'
+ *        500:
+ *          $ref: '#/components/responses/500'
+ */
+
+router.delete('/:id', isAuth, isAdmin, validateCategoryId, deleteCategory);
 
 module.exports = router;
--- routes/testimonies.js
+++ routes/testimonies.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const { isAdmin, isAuth } = require('../middlewares');
+
 const {
   getTestimonies,
   createTestimony,
@@ -12,11 +13,392 @@ const {
 
 const { validateCreateTestimony } = require('../validators/validateTestimony');
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Testimony
+ *     description: Users Testimonies
+ * components:
+ *   schemas:
+ *     Testimony Model:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: testimony name
+ *           required: true
+ *         content:
+ *           type: string
+ *           description: testimony content
+ *         image:
+ *           type: string
+ *           description: testimony image url
+ *         deletedAt:
+ *           type: date
+ *           description: delete date - softdelete
+ *         createdAt:
+ *           type: date
+ *           description: creation date
+ *         updatedAt:
+ *           type: date
+ *           description: update date
+ *       required:
+ *         - name
+ *         - content
+ *       example:
+ *         name: Testimony name
+ *         description: Testimony description
+ *         image: https://testimony-image.jpg
+ *     TstimonyResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: response title
+ *         data:
+ *           type: object
+ *           description: response content
+ *           properties:
+ *             id:
+ *               type: integer
+ *               description: testimony id
+ *             name:
+ *               type: string
+ *               description: testimony name
+ *             content:
+ *               type: string
+ *               description: testimony content
+ *             image:
+ *               type: string
+ *               description: testimony image url
+ *             createdAt:
+ *               type: date
+ *               description: creation date
+ *             updatedAt:
+ *               type: date
+ *               description: update date
+ *     TestimonyRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: testimony name
+ *           required: true
+ *         content:
+ *           type: string
+ *           description: testimony content
+ *           required: true
+ *         image:
+ *           type: string
+ *           description: testimony image url
+ *       example:
+ *         name: Testimony name
+ *         content: Testimony content
+ *         image: https://testimony-image.jpg
+ *     TestimonyPutRequest:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: testimony name
+ *         content:
+ *           type: string
+ *           description: testimony content
+ *         image:
+ *           type: string
+ *           description: testimony image url
+ *       example:
+ *         name: Testimony name
+ *         content: Testimony content
+ *         image: https://testimony-image.jpg
+ *     Testimonies:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: testimony id
+ *         name:
+ *           type: string
+ *           description: testimony name
+ *         content:
+ *           type: string
+ *           description: testimony content
+ *         image:
+ *           type: string
+ *           description: testimony image url
+ *         createdAt:
+ *           type: date
+ *           description: creation date
+ *         updatedAt:
+ *           type: date
+ *           description: update date
+ *     TestimoniesAndPagination:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: response title
+ *         data:
+ *           type: object
+ *           description: response content
+ *           properties:
+ *             totalPages:
+ *               type: integer
+ *               description: total number of response pages
+ *             previousPage:
+ *               type: integer
+ *               description: response previous page
+ *             currentPage:
+ *               type: integer
+ *               description: response current page
+ *             nextPage:
+ *               type: integer
+ *               description: response next page
+ *             totalRows:
+ *               type: integer
+ *               description: total number of response rows
+ *             rowsPerPage:
+ *               type: integer
+ *               description: number of response rows per page
+ *             rows:
+ *               type: array
+ *               description: current page rows
+ *               items:
+ *                 $ref: '#/components/schemas/Testimonies'
+ *     TestimonyMessageResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: message
+ *   responses:
+ *     getAllTestimonies:
+ *       description: list of all testimonies
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TestimoniesAndPagination'
+ *           example:
+ *             message: list of all testimonies
+ *             data:
+ *               totalPages: 1
+ *               previousPage: null
+ *               currentPage: 1
+ *               nextPage: null
+ *               totalRows: 2
+ *               rowsPerPage: 10
+ *               rows:
+ *                 - id: 1
+ *                   name: Testimony name
+ *                   content: Testimony content
+ *                   image: https://testimony-image.jpg
+ *                   createdAt: 2022-09-14T17:09:49.000Z
+ *                   updatedAt: 2022-09-14T17:09:49.000Z
+ *                 - id: 2
+ *                   name: Testimony name
+ *                   content: Testimony content
+ *                   image: https://testimony-image.jpg
+ *                   createdAt: 2022-09-14T17:09:49.000Z
+ *                   updatedAt: 2022-09-14T17:09:49.000Z
+ *     getSingleTestimony:
+ *       description: testimony detail
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TestimonyResponse'
+ *           example:
+ *             message: testimony detail
+ *             data:
+ *               id: 1
+ *               name: Testimony name
+ *               content: Testimony content
+ *               image: https://testimony-image.jpg
+ *               createdAt: 2022-09-14T17:09:49.000Z
+ *               updatedAt: 2022-09-14T17:09:49.000Z
+ *     deleteTestimony:
+ *       description: testimony deleted
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TestimonyMessageResponse'
+ *           example:
+ *             message: testimony deleted
+ *     createTestimony:
+ *       description: testimony created
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TestimonyResponse'
+ *           example:
+ *             message: testimony created
+ *             data:
+ *               id: 1
+ *               name: Testimony name
+ *               content: Testimony content
+ *               image: https://testimony-image.jpg
+ *               createdAt: 2022-09-14T17:09:49.000Z
+ *               updatedAt: 2022-09-14T17:09:49.000Z
+ *     updateTestimony:
+ *       description: testimony updated
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/TestimonyResponse'
+ *           example:
+ *             message: testimony updated
+ *             data:
+ *               id: 1
+ *               name: Testimony name
+ *               description: Testimony description
+ *               image: https://testimony-image.jpg
+ *               createdAt: 2022-09-14T17:09:49.000Z
+ *               updatedAt: 2022-09-14T17:09:49.000Z
+ *     notFoundTestimony:
+ *       description: testimony not found
+ *     badRequestTestimony:
+ *       description: testimony already exists
+ *     401:
+ *       description: unauthorized - id is required
+ *     403:
+ *       description: forbidden - admin access is required - validation error
+ *     500:
+ *       description: server error
+ *   parameters:
+ *     idTestimony:
+ *       in: path
+ *       name: id
+ *       schema:
+ *         type: integer
+ *       required: true
+ *       description: testimony id
+ *     page:
+ *       in: query
+ *       name: page
+ *       schema:
+ *         type: integer
+ *       description: page required
+ *     by:
+ *       in: query
+ *       name: by
+ *       schema:
+ *         type: string
+ *       description: criteria to order rows
+ *     order:
+ *       in: query
+ *       name: order
+ *       schema:
+ *         type: string
+ *       description: direction to order rows
+ *   securitySchemes:
+ *     ApiKeyAuth:
+ *       in: header
+ *       name: authorization
+ *       type: apiKey
+ */
+
+/**
+ * @swagger
+ * /testimonies:
+ *   get:
+ *     security:
+ *       - ApiKeyAuth: []
+ *     summary: Get all testimonies
+ *     tags: [Testimony]
+ *     parameters:
+ *     - $ref: '#/components/parameters/page'
+ *     - $ref: '#/components/parameters/by'
+ *     - $ref: '#/components/parameters/order'
+ *     responses:
+ *       200:
+ *         $ref: '#/components/responses/getAllTestimonies'
+ *       401:
+ *         $ref: '#/components/responses/401'
+ *       403:
+ *         $ref: '#/components/responses/403'
+ *       500:
+ *         $ref: '#/components/responses/500'
+ */
+
 router.get('/', isAuth, isAdmin, getTestimonies);
+
+/**
+ * @swagger
+ * /testimonies:
+ *    post:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Create a testimony
+ *      tags: [Testimony]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/TestimonyRequest'
+ *      responses:
+ *        201:
+ *          $ref: '#/components/responses/createTestimony'
+ *        400:
+ *          $ref: '#/components/responses/badRequestTestimony'
+ *        401:
+ *          $ref: '#/components/responses/401'
+ *        403:
+ *          $ref: '#/components/responses/403'
+ *        500:
+ *          $ref: '#/components/responses/500'
+ */
 
 router.post('/', isAuth, isAdmin, validateCreateTestimony, createTestimony);
 
+/**
+ * @swagger
+ * /testimonies/{id}:
+ *    put:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Update a testimony
+ *      tags: [Testimony]
+ *      parameters:
+ *      - $ref: '#/components/parameters/idTestimony'
+ *      requestBody:
+ *        required: true
+ *        content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/TestimonyPutRequest'
+ *      responses:
+ *        201:
+ *          $ref: '#/components/responses/updateTestimony'
+ *        401:
+ *          $ref: '#/components/responses/401'
+ *        403:
+ *          $ref: '#/components/responses/403'
+ *        500:
+ *          $ref: '#/components/responses/500'
+ */
+
 router.put('/:id', isAuth, isAdmin, updateTestimony);
+
+/**
+ * @swagger
+ * /testimonies/{id}:
+ *    delete:
+ *      security:
+ *        - ApiKeyAuth: []
+ *      summary: Delete a testimony
+ *      tags: [Testimony]
+ *      parameters:
+ *      - $ref: '#/components/parameters/idTestimony'
+ *      responses:
+ *        200:
+ *          $ref: '#/components/responses/deleteTestimony'
+ *        401:
+ *          $ref: '#/components/responses/401'
+ *        403:
+ *          $ref: '#/components/responses/403'
+ *        500:
+ *          $ref: '#/components/responses/500'
+ */
 
 router.delete('/:id', isAuth, isAdmin, deleteTestimony);
 
